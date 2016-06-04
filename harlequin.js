ba5b1b5461d317f3c59dd1f76072273e1205a7a1
Gregory McIntyre <greg@gregorymcintyre.com>
2016-06-04 20:19:37 +1000

    It works. Praise your gods and mine. Just kiding I'm godless.

--- harlequin.js
+++ harlequin.js
@@ -5,44 +5,32 @@ const debug = require('debug')('harlequin')
 class H {
     constructor() {
         this._context = [{ _cache: {} }]
-        this._indent = 0
     }
 
     code(fn) {
         this._code = fn
-        //console.log(this._indentPad() + fn)
-        this._incrementIndent()
+        console.log(chalk.grey(' code:'), fn)
         return this
     }
 
     context(newContext) {
+        console.log(chalk.grey(' context:'), newContext)
         var ctx = this._currentContext()
+        debug('CONTEXT BEFORE ADD:', ctx)
         for (let k in newContext) {
+            ctx = this._context[this._context.length - 1] = this._cloneObject(ctx, k)
+            debug('CONTEXT BEFORE CLONE:', ctx)
             var val = newContext[k]
-            if (typeof val === 'function') {
-                debug('Adding property ' + k + ' to context', ctx, ' with value ' + val)
-                Object.defineProperty(ctx, k, {
-                    get: function () {
-                        return this._cache[k] = this._cache[k] || val()
-                    },
-                    enumerable: true
-                })
-            } else {
-                //this._currentContext()[k] = val
-                Object.defineProperty(ctx, k, {
-                    value: val,
-                    enumerable: true
-                })
-            }
+            this._addValueToContext(ctx, k, val)
+            debug('CONTEXT AFTER CLONE:', ctx)
         }
-        //console.log(this._indentPad(), newContext)
-        this._incrementIndent()
+        debug('CONTEXT AFTER ADD:', this._currentContext())
         return this
     }
 
     test(note) {
         this._push()
-        this._note = note
+        console.log(note)
         return this
     }
 
@@ -52,11 +40,7 @@ class H {
     }
 
     returns(value) {
-        this._note = this._note || [
-            this._indentPad(),
-            "returns",
-            value
-        ].join(' ')
+        this._testNote = [ "returns", value ]
         let result = this._execute()
         this._assertEqual(result, value)
         this._pop()
@@ -69,14 +53,7 @@ class H {
     }
 
     by(val) {
-        this._note = this._note || [
-            this._indentPad(),
-            "changes",
-            this._changesCodeTest,
-            "by",
-            val
-        ].join(' ')
-
+        this._testNote = [ "changes", this._changesCodeTest, "by", val ]
         let before = this._execute(this._changesCodeTest)
         let result = this._execute()
         let after = this._execute(this._changesCodeTest)
@@ -86,68 +63,80 @@ class H {
     }
 
     to(val) {
-        this._note = this._note || [
-            this._indentPad(),
-            "changes",
-            this._changesCodeTest,
-            "to",
-            val
-        ].join(' ')
-        let code = [
-            this._code,
-            "var after = " + this._changesCodeTest,
-            "after"
-        ].join("\n")
-        let after = this._execute(code)
+        this._testNote = [ "changes", this._changesCodeTest, "to", val ]
+        let result = this._execute()
+        let after = this._execute(this._changesCodeTest)
         this._assertEqual(after, val)
         this._pop()
         return this
+    }
+
+    _addValueToContext(ctx, k, val) {
+        debug('ADDING ' + k + ': ' + val)
+        if (typeof val === 'function') {
+            debug('ADDING PROPERTY ' + k)
+            Object.defineProperty(ctx, k, {
+                get: function () { return this._cache[k] = this._cache[k] || val() },
+                enumerable: true,
+                writeable: true
+            })
+        } else {
+            ctx[k] = val
+        }
     }
 
     _currentContext() {
         return this._context[this._context.length - 1]
     }
 
+    _cloneObject(obj, except = undefined) {
+        var msg = except ? 'CLONE except "' + except + '" key' : 'CLONE all keys'
+        debug(msg, obj)
+        let newObj = {}
+        for (let k in obj) {
+            if (k === except) { continue }
+            let getter = obj.__lookupGetter__(k)
+            if (getter) {
+                Object.defineProperty(newObj, k, {get: getter, enumerable: true, writeable: true})
+            } else {
+                newObj[k] = obj[k]
+            }
+        }
+        newObj._cache = {}
+        debug('CLONE result is', obj)
+        return newObj
+    }
+
     _push() {
-        var newContext = Object.assign({}, this._currentContext(), {_cache: {}})
-        //newContext = this._currentContext()
+        debug("PUSH", this._currentContext())
+        let newContext = this._cloneObject(this._currentContext())
         this._context.push(newContext)
     }
 
     _pop() {
         this._context.pop()
-        this._indent -= 1
-    }
-
-    _incrementIndent() {
-        this._indent += 1
-    }
-
-    _indentPad() {
-        let result = []
-        for (let i = 0; i < this._indent; i++) {
-            result.push('    ')
-        }
-        return result.join('')
     }
 
     _assertEqual(actual, expected) {
         let pass = actual === expected
         if (pass) {
-            console.log(chalk.green(this._note))
+            console.log(chalk.green('', this._testNote.join(' ')))
         } else {
-            console.log(chalk.red(this._note, actual, '!==', expected))
+            console.log(chalk.red('', this._testNote.join(' '), '(', actual, '!==', expected, ')'))
         }
         this._note = undefined
     }
 
     _execute(code = this._code) {
 
-        // This is a nasty hack to enable re-binding => functions
-        // TODO: field test this better, maybe a different solution
-
         let ctx = this._currentContext()
-        debug('Executing code. context:', ctx, 'code:', code, ctx.sum)
+        debug("EXEC")
+        debug("     context: ", ctx)
+        debug("     code:", code)
+
+        // This is a nasty hack to enable re-binding arrow functions
+        //
+        // TODO: field test this better, maybe a different solution
 
         let wrapper = function() {
             let localCode = eval(String(code))
--- test.sum.js
+++ test.sum.js
@@ -7,60 +7,25 @@ harlequin
     .code(() => this.sum.calc(this.a, this.b, this.c))
     .context({ sum: () => new Sum() })                 // NB: needs scope from this file, eval solution doesn't work
 
-    .test("Adds to six")                               // push
-        .context({ a: 1, b: 2, c: 3 })                 //
-        .returns(6)                                    // pop (returns)
+    .test("Adds to six")
+        .context({ a: 1, b: 2, c: 3 })
+        .returns(6)
 
-    .test("Adds to seven")                             // push (test)
-        .context({ a: 2, b: 2, c: 3 })                 // push
-        .returns(8)                                    // pop (returns) - failing test
-
-    .test("Changes times summed")                      // push (test)
+    .test("Adds to seven")
         .context({ a: 2, b: 2, c: 3 })
-        .changes(() => this.sum.timesSummed).by(1)     // pop (by)
+        .returns(8) // oops
+
+    .test("Changes times summed")
+        .context({ a: 2, b: 2, c: 3 })
+        .changes(() => this.sum.timesSummed).by(1)
 
     .test("Initialises times summed")
-        .context({ sum: () => new Sum(10) })           // cannot re-define sum ?? :(
+        .context({ sum: () => new Sum(10) })
         .code(() => this.sum.timesSummed)
         .returns(10)
 
     .test("Adds to initialised sum")
-        .context({ sum: () => new Sum(10) })           // cannot re-define sum ?? :(
+        .context({ sum: () => new Sum(10) })
         .context({ a: 2, b: 2, c: 3 })
         .code(() => this.sum.calc(this.a, this.b, this.c) && this.sum.timesSummed)
         .returns(11)
-
-
-    // .test("Changes times summed")
-    //     .changes(() => this.sum.timesSummed).by(1)
-
-// new H()
-//     .code(()=> sum.calc(a, b, c))
-//     .context(()=>{ sum = new Sum() })
-//     .context(()=>{ a = 1, b = 2, c = 3 })
-//         .returns(6)
-//     .context(()=>{ a = 2, b = 2, c = 3 })
-//         .returns(7)
-//     .changes(()=> sum.timesSummed).by(1)
-
-
-// harlequin
-//     .code(() => this.sum.calc(this.a, this.b, this.c))
-//     .context({sum: () => new Sum()})
-//     .push()
-//         .note("adds numbers")
-//         .push()
-//             .context({a: 1, b: 2, c: 3})
-//             .returns(6)
-//         .pop()
-//         .push()
-//             .context({a: 2, b: 2, c: 3}) // error
-//             .returns(7)
-//         .pop()
-//     .pop()
-//     .push()
-//         .note("initialises times summed")
-//         .context({sum: () => new Sum(10)})
-//         .code(() => this.sum.timesSummed)
-//         .returns(10)
-//         .pop()
